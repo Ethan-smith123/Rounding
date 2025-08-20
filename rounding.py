b80fc396f2f4458f14935acc1e35819f77666b48
Ethan-smith123 <ethans.23118@obhs.school.nz>
2025-08-21 09:08:15 +1200

    Add files via upload
    Rounding methods I could use in the assessment

---
+++ rounding.py
@@ -0,0 +1,14 @@
+if __main__ ='__main__'
+total = (10.283 + 7.6991)/ 2.66
+print ('this is the total',total)
+print (round(total, 2 ))
+
+# or i could do this 
+
+total = (10.283 + 7.6991)/ 2.66
+print(f'this is the total {total:2f}',round(total,2)) #display as a float of a number
+
+# string formatting method... slicing...
+# word = butter
+# word [2:5]
+#['a','b','c'.'d'] #[3]
